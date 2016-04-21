3c14e7aa4af0f411736c5e8dc005544ba91a71c7
VamsiPusapati <vamsi.pusapati2014@gmail.com>
2016-04-21 11:15:06 -0300

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,20 @@
+
+
+(function () {
+    var die1 = document.getElementById("die1");
+    var die2 = document.getElementById("die2");
+    var die1Label = document.getElementById("die1Label");
+    var die2Label = document.getElementById("die2Label");
+
+    $('#roll').click(function(){
+        var die1roll = Math.floor((Math.random() * 6) + 1);
+        var die2roll = Math.floor((Math.random() * 6) + 1);
+        
+        die1Label.innerHTML = die1roll;
+        die2Label.innerHTML = die2roll;
+        
+        die1.setAttribute("src",("../Assets/images/" + die1roll + ".png"));
+        die2.setAttribute("src",("../Assets/images/" + die2roll + ".png"));
+    });
+    
+})();
